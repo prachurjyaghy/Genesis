8ef6feec0c6daa57a72c7692135e095b4901fcc7
prachurjyaghy <48976708+prachurjyaghy@users.noreply.github.com>
2020-04-07 06:42:10 +0530

    new again

--- tests.py
+++ tests.py
@@ -1,22 +1,22 @@
-# Python 3.x code to demonstrate star pattern 
-  
-# Function to demonstrate printing pattern 
-def pypart(n): 
-      
-    # outer loop to handle number of rows 
-    # n in this case 
-    for i in range(0, n): 
-      
-        # inner loop to handle number of columns 
-        # values changing acc. to outer loop 
-        for j in range(0, i+1): 
-          
-            # printing stars 
-            print("* ",end="") 
+# Program to check if a number is prime or not
+
+num = 407
+
+# To take input from the user
+#num = int(input("Enter a number: "))
+
+# prime numbers are greater than 1
+if num > 1:
+   # check for factors
+   for i in range(2,num):
+       if (num % i) == 0:
+           print(num,"is not a prime number")
+           print(i,"times",num//i,"is",num)
+           break
+   else:
+       print(num,"is a prime number")
        
-        # ending line after each row 
-        print("\r") 
-  
-# Driver Code 
-n = 5
-pypart(n)
+# if input number is less than
+# or equal to 1, it is not prime
+else:
+   print(num,"is not a prime number")
